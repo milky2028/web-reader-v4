6ec16d62ef2f9bf8ea2f5efe7372a93a3faec86c
Tyler <tylergross28@gmail.com>
2024-06-01 12:18:06 -0400

    just cleanup

--- functions/src/getPage.ts
+++ functions/src/getPage.ts
@@ -6,16 +6,16 @@ import { type Request } from 'express';
 
 export const getPage = onRequest(async (req: Request, res) => {
 	const { book, cover } = req.query;
-	const authHeader = req.headers.authorization;
-	if (!book || !cover || !authHeader) {
+	const { authorization } = req.headers;
+	if (!book || !cover || !authorization) {
 		res
 			.status(500)
-			.send(`Missing request parameters. Book: ${book}, Cover: ${cover}, Auth: ${authHeader}`);
+			.send(`Missing request parameters. Book: ${book}, Cover: ${cover}, Auth: ${authorization}`);
 		return;
 	}
 
 	try {
-		const decodedToken = await auth.verifyIdToken(authHeader);
+		const decodedToken = await auth.verifyIdToken(authorization);
 		const bucket = getStorage(app).bucket();
 		const [file] = await bucket.file(`${decodedToken.uid}/${book}/${cover}`).download();
 		const responseHeaders = {
