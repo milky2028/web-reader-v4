35aca17f49ef622c70d28dcd41ac4d7ee320663a
Tyler <tylergross28@gmail.com>
2024-06-01 12:00:50 -0400

    try to catch erro

--- functions/src/getPage.ts
+++ functions/src/getPage.ts
@@ -14,14 +14,21 @@ export const getPage = onRequest(async (req: Request<{ book?: string; cover?: st
 		return;
 	}
 
-	const decodedToken = await auth.verifyIdToken(authHeader);
-	const bucket = getStorage(app).bucket();
-	const [file] = await bucket.file(`${decodedToken.uid}/${book}/${cover}`).download();
-	const responseHeaders = {
-		'Content-Type': 'image/jpeg',
-		'Cache-Control': 'public, max-age=31536000, s-maxage=31536000'
-	};
+	try {
+		const decodedToken = await auth.verifyIdToken(authHeader);
+		const bucket = getStorage(app).bucket();
+		const [file] = await bucket.file(`${decodedToken.uid}/${book}/${cover}`).download();
+		const responseHeaders = {
+			'Content-Type': 'image/jpeg',
+			'Cache-Control': 'public, max-age=31536000, s-maxage=31536000'
+		};
 
-	res.status(200).header(responseHeaders).send(file);
-	return;
+		res.status(200).header(responseHeaders).send(file);
+	} catch (e) {
+		if (e instanceof Error) {
+			res.status(500).send(e.message);
+		} else {
+			res.status(500).send(e);
+		}
+	}
 });
