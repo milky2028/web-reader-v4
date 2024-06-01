1d56c9ef8ec1879cf13ce636cc7c19bfa76e5182
Tyler <tylergross28@gmail.com>
2024-06-01 11:30:50 -0400

    fix linting

--- functions/.eslintrc.cjs
+++ functions/.eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		project: ['tsconfig.json', 'tsconfig.dev.json'],
+		project: ['./functions/tsconfig.json'],
 		sourceType: 'module'
 	},
 	ignorePatterns: [
