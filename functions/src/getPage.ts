eb3229b9c4e1850e2e987a3587f6372c2ea4b692
Tyler <tylergross28@gmail.com>
2024-06-01 11:18:57 -0400

    get page function is done!

--- functions/src/getPage.ts
+++ functions/src/getPage.ts
@@ -1,5 +1,27 @@
-import { onRequest } from "firebase-functions/v2/https";
+import { onRequest } from 'firebase-functions/v2/https';
+import { getStorage } from 'firebase-admin/storage';
+import { auth } from './auth';
+import { app } from './app';
+import { type Request } from 'express';
 
-export const getPage = onRequest(async (_req, res) => {
-  res.status(200).send("It works?");
+export const getPage = onRequest(async (req: Request<{ book?: string; cover?: string }>, res) => {
+  const { book, cover } = req.body;
+  const authHeader = req.headers.authorization;
+  if (!book || !cover || !authHeader) {
+    res
+      .status(500)
+      .send(`Missing request paremeters. Book: ${book}, Cover: ${cover}, Auth: ${authHeader}`);
+    return;
+  }
+
+  const decodedToken = await auth.verifyIdToken(authHeader);
+  const bucket = getStorage(app).bucket();
+  const [file] = await bucket.file(`${decodedToken.uid}/${book}/${cover}`).download();
+  const responseHeaders = {
+    'Content-Type': 'image/jpeg',
+    'Cache-Control': 'public, max-age=31536000, s-maxage=31536000'
+  };
+
+  res.status(200).header(responseHeaders).send(file);
+  return;
 });
