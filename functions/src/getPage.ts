5ee52e57fcfccef541a47056b153972a24fc1412
Tyler <tylergross28@gmail.com>
2024-06-01 11:39:54 -0400

    working get page function

--- functions/src/getPage.ts
+++ functions/src/getPage.ts
@@ -5,23 +5,23 @@ import { app } from './app';
 import { type Request } from 'express';
 
 export const getPage = onRequest(async (req: Request<{ book?: string; cover?: string }>, res) => {
-  const { book, cover } = req.body;
-  const authHeader = req.headers.authorization;
-  if (!book || !cover || !authHeader) {
-    res
-      .status(500)
-      .send(`Missing request paremeters. Book: ${book}, Cover: ${cover}, Auth: ${authHeader}`);
-    return;
-  }
+	const { book, cover } = req.body;
+	const authHeader = req.headers.authorization;
+	if (!book || !cover || !authHeader) {
+		res
+			.status(500)
+			.send(`Missing request paremeters. Book: ${book}, Cover: ${cover}, Auth: ${authHeader}`);
+		return;
+	}
 
-  const decodedToken = await auth.verifyIdToken(authHeader);
-  const bucket = getStorage(app).bucket();
-  const [file] = await bucket.file(`${decodedToken.uid}/${book}/${cover}`).download();
-  const responseHeaders = {
-    'Content-Type': 'image/jpeg',
-    'Cache-Control': 'public, max-age=31536000, s-maxage=31536000'
-  };
+	const decodedToken = await auth.verifyIdToken(authHeader);
+	const bucket = getStorage(app).bucket();
+	const [file] = await bucket.file(`${decodedToken.uid}/${book}/${cover}`).download();
+	const responseHeaders = {
+		'Content-Type': 'image/jpeg',
+		'Cache-Control': 'public, max-age=31536000, s-maxage=31536000'
+	};
 
-  res.status(200).header(responseHeaders).send(file);
-  return;
+	res.status(200).header(responseHeaders).send(file);
+	return;
 });
