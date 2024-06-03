b0588c40eb920d002f64318e0bb86691bd7f836a
Tyler <tylergross28@gmail.com>
2024-06-03 01:24:21 -0400

    add function base for resizing image

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import { getPage as _getPage } from './getPage';
 import { getBooks as _getBooks } from './getBooks';
+import { resizeImage as _resizeImage } from './resizeImage';
 
 export const getPage = _getPage;
 export const getBooks = _getBooks;
+export const resizeImage = _resizeImage;
---
+++ functions/src/resizeImage.ts
@@ -0,0 +1,3 @@
+import { onObjectFinalized } from 'firebase-functions/v2/storage';
+
+export const resizeImage = onObjectFinalized(() => {});
