366dfb7be7707e22dd3ab0c100aca769133bea2d
Tyler <tylergross28@gmail.com>
2024-06-03 11:15:19 -0400

    fix detached buffer

--- app/src/lib/allocateFileStreaming.ts
+++ app/src/lib/allocateFileStreaming.ts
@@ -1,9 +1,9 @@
-function createMemoryWritable(heap: Uint8Array, startingPtr: number) {
+function createMemoryWritable(wasm: Awaited<typeof import('$lib/wasm').wasm>, startingPtr: number) {
 	let position = startingPtr;
 
 	return new WritableStream<Uint8Array>({
 		write(chunk) {
-			heap.set(chunk, position);
+			wasm.HEAPU8.set(chunk, position);
 			position += chunk.byteLength;
 		}
 	});
@@ -12,7 +12,7 @@ function createMemoryWritable(heap: Uint8Array, startingPtr: number) {
 export async function allocateFileStreaming(file: File) {
 	const wasm = await import('$lib/wasm').then(({ wasm }) => wasm);
 	const ptr = wasm._malloc(file.size);
-	await file.stream().pipeTo(createMemoryWritable(wasm.HEAPU8, ptr));
+	await file.stream().pipeTo(createMemoryWritable(wasm, ptr));
 
 	return {
 		ptr,
