a6f8768a29debb3f610879f2fc74cf62b7abbe83
Tyler <tylergross28@gmail.com>
2024-06-10 16:06:27 -0400

    make sure this only happens once

--- app/src/lib/mountWasmFilesystem.ts
+++ app/src/lib/mountWasmFilesystem.ts
@@ -1,7 +1,20 @@
+let mounted = false;
+
 export function mountWasmFilesystem(wasm: Awaited<typeof import('$lib/wasm').wasm>) {
 	return new Promise<void>((resolve) => {
-		const jobId = crypto.randomUUID();
-		window.addEventListener(jobId, () => resolve(), { once: true });
-		wasm.mount_filesystem(jobId);
+		if (!mounted) {
+			const jobId = crypto.randomUUID();
+			window.addEventListener(
+				jobId,
+				() => {
+					resolve();
+					mounted = true;
+				},
+				{ once: true }
+			);
+			wasm.mount_filesystem(jobId);
+		}
+
+		resolve();
 	});
 }
