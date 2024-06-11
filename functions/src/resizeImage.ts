c269742efd29dfd7417d1b1806fee278ef085363
Tyler <tylergross28@gmail.com>
2024-06-10 20:43:45 -0400

    optimize image on upload

--- functions/src/resizeImage.ts
+++ functions/src/resizeImage.ts
@@ -1,3 +1,19 @@
 import { onObjectFinalized } from 'firebase-functions/v2/storage';
+import { storage } from './storage';
+import sharp from 'sharp';
 
-export const resizeImage = onObjectFinalized(() => {});
+function isImage(type: string | undefined) {
+	return type?.startsWith('image/');
+}
+
+export const resizeImage = onObjectFinalized(async (event) => {
+	if (isImage(event.data.contentType)) {
+		const { bucket, name: path, contentType } = event.data;
+		const [file] = await storage.bucket(bucket).file(path).download();
+		const optimizedBuffer = await sharp(file).jpeg({ mozjpeg: true, quality: 75 }).toBuffer();
+		await storage
+			.bucket(event.data.bucket)
+			.file(path)
+			.save(optimizedBuffer, { metadata: { contentType } });
+	}
+});
