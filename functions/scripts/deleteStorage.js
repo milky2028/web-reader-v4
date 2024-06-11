44db2033806472182426b7f0df5f5040d58c5b34
Tyler <tylergross28@gmail.com>
2024-06-10 20:58:48 -0400

    add deleter function

---
+++ functions/scripts/deleteStorage.js
@@ -0,0 +1,14 @@
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getStorage } = require('firebase-admin/storage');
+const serviceAccount = require('./cert.json');
+
+const app = initializeApp({
+	credential: cert(serviceAccount),
+	storageBucket: 'gs://web-reader-ae90f.appspot.com'
+});
+
+const bucket = getStorage(app).bucket();
+bucket.deleteFiles({ prefix: 'P1rHmhJ80KOevyB3EmxkyyEzUGj1' });
+
+// 9JSEgkf1lAULxvE8xShxILYt4n62/
+// P1rHmhJ80KOevyB3EmxkyyEzUGj1/
