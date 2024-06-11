defde7a958dc2e034c55fa7756fcf7bd012113e3
Tyler <tylergross28@gmail.com>
2024-06-10 22:56:15 -0400

    maybe this will work?

--- functions/src/resizeImage.ts
+++ functions/src/resizeImage.ts
@@ -9,11 +9,12 @@ function isImage(type: string | undefined) {
 export const resizeImage = onObjectFinalized({ memory: '2GiB', cpu: 4 }, async (event) => {
 	if (isImage(event.data.contentType)) {
 		const { bucket, name: path, contentType } = event.data;
-		const [file] = await storage.bucket(bucket).file(path).download();
-		const optimizedBuffer = await sharp(file).jpeg({ mozjpeg: true, quality: 75 }).toBuffer();
+		const file = storage.bucket(bucket).file(path);
+		const [buffer] = await file.download();
+		const optimizedBuffer = await sharp(buffer).jpeg({ mozjpeg: true, quality: 75 }).toBuffer();
 		await storage
 			.bucket(event.data.bucket)
 			.file(path)
-			.save(optimizedBuffer, { metadata: { contentType, resized: true } });
+			.save(optimizedBuffer, { metadata: { contentType, customMetadata: { resized: true } } });
 	}
 });
